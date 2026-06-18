bf3bd97bceb23df03d00dc69ba88fd1ca5ceba35
Toshiki Ohshima <ohsima-toshiki-bg@ynu.jp>
2026-06-18 16:12:23 +0900

    Tune GamBallRun gameplay and materials

--- Assets/Scripts/StageTilt.cs
+++ Assets/Scripts/StageTilt.cs
@@ -1,42 +1,23 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class StageTilt : MonoBehaviour
 {
-    public float tiltSpeed = 24f;
-    public float maxTilt = 16f;
+    [SerializeField] private float maxTilt = 24f;
+    [SerializeField] private float tiltSpeed = 2f;
+    [SerializeField] private float tiltForward = 6f;
 
-
-    
     void Update()
     {
-        float horizontal = 0f;
-        float vertical = 0f;
-        
-        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1f;
-        }
-        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1f;
-        }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = -1f;
-        }
-        else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = 1f;
-        }
+        Quaternion targetRotation = Quaternion.Euler(
+            horizontal * maxTilt,
+            0f,
+            tiltForward
+        );
 
-        float xTilt = horizontal * maxTilt;
-        float zTilt = -vertical * maxTilt;
-
-        Quaternion targetRotation = Quaternion.Euler(xTilt, 0f, zTilt);
-
-        transform.rotation = Quaternion.RotateTowards(
+        transform.rotation = Quaternion.Lerp(
             transform.rotation,
             targetRotation,
             tiltSpeed * Time.deltaTime
